f080de42ac1507b32732d74341fb5614b7ea26fc
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-01-22 00:24:49 +0545

    Create migrations for the created model employee,

--- crudexample/crudexample/settings.py
+++ crudexample/crudexample/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee'
 ]
 
 MIDDLEWARE = [
@@ -75,12 +76,14 @@ WSGI_APPLICATION = 'crudexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'djangodb',  
-        'USER':'root',  
-        'PASSWORD':'',  
-        'HOST':'localhost',  
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',  
+        # 'NAME': 'djangodb',  
+        # 'USER':'root',  
+        # 'PASSWORD':'',  
+        # 'HOST':'localhost',  
+        # 'PORT':'3306'
     }
 }
 
