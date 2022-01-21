9fa6c646b4e7037702a076ee18552e90e4b6a489
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-01-21 23:44:30 +0545

    3) Database Setup

--- crudexample/crudexample/settings.py
+++ crudexample/crudexample/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crudexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangodb',  
+        'USER':'root',  
+        'PASSWORD':'',  
+        'HOST':'localhost',  
+        'PORT':'3306'
     }
 }
 
